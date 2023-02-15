a010b800873074dd8499d2e66d49a329a307e875
zisuzin <zi_soo_zin@naver.com>
2023-02-16 00:59:21 +0900

    s

--- 2.to-do-list/todo1.js
+++ 2.to-do-list/todo1.js
@@ -1,34 +1,57 @@
-const qs = document.querySelector(x);
-const qsa = document.querySelectorAll(x);
 
-const itemList = [];
-const btn = qs(".input__button"); // 추가 버튼
+"use-strict";
+
+let itList = [];
+const btn = document.querySelector(".input__button");
 btn.addEventListener("click",itemAdd);
 
 function itemAdd(){
-    const item = document.querySelector(".item").value;
-    if(item !== "null"){
-        itemList.push(item);
+    let item = document.querySelector(".item").value;
+    if(item !== null){
+        itList.push(item);
         document.querySelector(".item").value = "";
         document.querySelector(".item").focus();
     }
-} showList();
+    listAdd();
+} 
 
-function showList(){
-    const List = "<ul>";
-    for(i=0; i<itemList.value; i++){
-        List += "<li>" + "<span class='closed' id="+ i + ">" + "\u00D7" + "</span></li>";
+function listAdd(){
+    let list = "<ul>";
+    for(let i=0; i<itList.length; i++){
+        list += "<li>" + itList[i] + "<span class='close' id=i" + ">" + "\u2717" + "</span></li>";
     }
-    List += "</ul>";
-    document.querySelector(".item__list").innerHTML = List;
+    list += "</ul>"
+    document.querySelector(".item__list").innerHTML = list;
 
-    const deleteItem = document.querySelector(".closed");
-    for(let i=0; i<deleteItem.length; i++){
-        deleteItem[i].addEventListener("click",deleteButtons);
+    let close = document.querySelectorAll(".close");
+    for(let i=0; i<close.length; i++){
+        close[i].addEventListener("click",deleteButtons);
     }
-}
+}   
 
 function deleteButtons(){
     let id = this.getAttribute("id");
     itemList.splice(id,1);
-}
+}   listAdd();
+
+const checkList = document.querySelector(".item__list");
+checkList.addEventListener("click",event=>{
+    if(event.target.tagName === "LI"){
+        event.target.classList.toggle("checked");
+    }
+});
+
+
+// 타이핑 효과 적용
+let content = "To do list"; 
+let tit = document.querySelector(".main__title h1");
+let idx = 0;
+
+function typ(){
+    tit.innerHTML += content[idx++];
+    if(idx>content.length){
+        tit.innerHTML = "";
+        idx = 0;
+    }
+} 
+setInterval(typ,200);
