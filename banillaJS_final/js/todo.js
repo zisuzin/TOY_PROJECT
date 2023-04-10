1410e96ded97b8ba7b0559b0744fa6d5245fb4e2
zisuzin <zi_soo_zin@naver.com>
2023-04-11 04:50:54 +0900

    s

--- banillaJS_final/js/todo.js
+++ banillaJS_final/js/todo.js
@@ -1,51 +1,47 @@
 
-const form = document.querySelector(".todoWrite");
-const formInput = form.querySelector("input");
-const todoList = document.querySelector(".Todo_List ul");
+const todos = [];
+const submitBtn = document.querySelector(".todoWrite button");
+// console.log(submitBtn)
 
-const arr = [];
+submitBtn.addEventListener("click", addList);
 
-    function userWrite(e){
-        e.preventDefault();
-        let newTodo = formInput.value;
-        formInput.value = "";
-        // console.log(newTodo,formInput.value);
-        arr.push(newTodo);
-        paintToDo(newTodo);
+// 배열추가 함수
+function addList() {
+    let inputv = document.querySelector(".todoWrite input").value;
+    if(inputv != null) {
+        todos.push(inputv);
+        // console.log(todos)
+        document.querySelector(".todoWrite input").value = "";
+        document.querySelector(".todoWrite input").focus();
     }
-    
-    form.addEventListener("submit",userWrite);
-    
-    function paintToDo(newTodo){
-        const li = document.createElement("li");
-        const span = document.createElement("span");
+    createList();
+}
 
-        todoList.appendChild(li);
-        li.appendChild(span);
-        span.innerText = newTodo;
-
+// 리스트생성 함수
+function createList() {
+    let hcode = "<ul>"
+    for(let i=0; i<todos.length; i++) {
+        hcode += "<li>" + todos[i] + "<span class='close' id="+ i +">" + "<i class='fa-regular fa-trash-can'>" + "\u00D7" + "</i></span></li>";
     }
+    hcode += "</ul>";
+    document.querySelector(".Todo_List").innerHTML = hcode;
 
-    function paintToDo(newTodo){
-        // console.log("I will paint",newTodo);
-        const li = document.createElement("li");
-        const span = document.createElement("span");
-        span.innerText = newTodo;
-        const button = document.createElement("button");
-        // console.log(li);
-        toDoList.appendChild(li);
-        li.appendChild(span);
-        button.innerText= "X";
-        li.appendChild(button);
-        button.addEventListener("click",deleteToDo);
-        // console.log(toDoList);
-    ;}
-    
-    
-    function deleteToDo(event){
-        const li = event.target.parentElement;
-        // li.style.textDecoration = "line-through";
-        li.remove();
-        saveToDos();
+    let deletebtn = document.querySelectorAll(".close");
+    for(let i=0; i<deletebtn.length; i++) {
+        deletebtn[i].addEventListener("click",deleteList);
     }
-    
+}
+
+// 리스트삭제 함수
+function deleteList() {
+    let id = this.getAttribute("id");
+    todos.splice(id,1);
+    createList();
+}
+
+let checktodo = document.querySelector(".Todo_List");
+checktodo.addEventListener("click", (e) => {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle('checked');
+    }
+});
