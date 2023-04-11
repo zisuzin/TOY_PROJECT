993e464233582ff47d0c05797190d82f4c1b1221
zisuzin <zi_soo_zin@naver.com>
2023-04-11 13:04:38 +0900

    S

--- banillaJS_final/js/google_search.js
+++ banillaJS_final/js/google_search.js
@@ -5,8 +5,11 @@ const serachInput = document.querySelector("#search-form input");
 
 function searchSubmit(e) {
     event.preventDefault();
+    // GET방식 도달페이지 연결  
     const link = `https://www.google.com/search?q=${serachInput.value}`;
-    location.href = link;
+    // location.href = link; - 현재페이지에서 열기
+    // window.open(주소) - 새창에서 열기
+    window.open(link);
 
     // 사용자 입력값이 있거나 공백포함인 경우 입력창 초기화
     setTimeout(()=>{
