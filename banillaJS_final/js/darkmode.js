ce93e677d4663a6af82184907474ee85d3ad4e14
zisuzin <zi_soo_zin@naver.com>
2023-04-11 13:20:16 +0900

    s

--- banillaJS_final/js/darkmode.js
+++ banillaJS_final/js/darkmode.js
@@ -1,5 +1,13 @@
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    // 다크모드 처리
-  } else {
-    // 라이트모드 처리
-  }
+
+const darkbtn = document.querySelector("#dark_mode_btn");
+const container = document.querySelector("#container");
+const contbox = document.querySelector(".cont");
+
+darkbtn.addEventListener("click",() => {
+  // console.log("클릭");
+  container.style.backgroundColor = "#000";
+  contbox.style.boxShadow = "none";
+  darkbtn.style.width = "144px";
+  darkbtn.style.backgroundPosition = "-714px 0px";
+  // background-position: -714px 0px;
+}); 
