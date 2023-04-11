6ffba23d768d03bb8b9888dd131ea005f68432fa
zisuzin <zi_soo_zin@naver.com>
2023-04-11 14:38:17 +0900

    s

--- banillaJS_final/js/darkmode.js
+++ banillaJS_final/js/darkmode.js
@@ -1,5 +1,6 @@
 
 const darkbtn = document.querySelector("#dark_mode_btn");
+const whitebtn = document.querySelector(".on");
 const container = document.querySelector("#container");
 const contbox = document.querySelector(".cont");
 
@@ -7,7 +8,10 @@ darkbtn.addEventListener("click",() => {
   // console.log("클릭");
   container.style.backgroundColor = "#000";
   contbox.style.boxShadow = "none";
-  darkbtn.style.width = "144px";
-  darkbtn.style.backgroundPosition = "-714px 0px";
-  // background-position: -714px 0px;
+  darkbtn.classList.toggle("on");
 }); 
+
+// whitebtn.addEventListener("click",() => {
+//   // container.style.backgroundColor = "#fff";
+//   // darkbtn.style.width = "136px";
+// });
