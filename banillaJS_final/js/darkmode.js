3e8a0554d85a13cdd06ccb337ffaf0e34ad7aea2
zisuzin <zi_soo_zin@naver.com>
2023-04-11 19:39:41 +0900

    다크모드기능 추가

--- banillaJS_final/js/darkmode.js
+++ banillaJS_final/js/darkmode.js
@@ -1,12 +1,29 @@
 
 const darkbtn = document.querySelector("#dark_mode_btn");
-const whitebtn = document.querySelector(".on");
 const container = document.querySelector("#container");
 const contbox = document.querySelector(".cont");
+const title = document.querySelectorAll(".tit");
+const contA = document.querySelectorAll(".contbx");
+const dates = document.querySelectorAll(".dates");
 
-darkbtn.addEventListener("click",() => {
-  // console.log("클릭");
+darkbtn.addEventListener("click",(e) => {
   container.classList.toggle("on");
   contbox.classList.toggle("on");
   darkbtn.classList.toggle("on");
+  eachEle(e);
 }); 
+
+
+function eachEle() {
+  title.forEach((ele) => {
+    ele.classList.toggle("on");
+  });
+
+  contA.forEach((ele) => {
+    ele.classList.toggle("on");
+  })
+
+  dates.forEach((ele) => {
+    ele.classList.toggle("on");
+  })
+}
